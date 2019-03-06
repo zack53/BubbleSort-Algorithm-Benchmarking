14c773e87fa3baf5199cf6d1e427fa2135e6188d
zack53 <48144629+zack53@users.noreply.github.com>
2019-03-05 22:56:39 -0500

    Update SortMain.java

--- Source/SortMain.java
+++ Source/SortMain.java
@@ -12,23 +12,24 @@ public class SortMain extends JFrame{
     private static JFrame frame = new JFrame("Benchmarking Data");
     private static JPanel myJPanel = new JPanel(new GridBagLayout());
     private  static JTextArea outputField = new JTextArea();
+    private static int warmupNum = 200;
     
     public static void main(String[] args)  {
         //data set sizes
         int[] sizes = new int[]{5,10,20,50,100,200,350,600,1000,1500};
         System.out.println("Manual JVM warmup in progress, 200 to complete.");
         //manual warmup with the last iteration being displayed to screen
-        for (int i = 1; i <= 201; i++) {
+        for (int i = 1; i <= warmupNum; i++) {
             BenchmarkSorts sortMain = new BenchmarkSorts(sizes);
             try {
                 sortMain.runSorts();
             } catch (UnsortedException ex) {
                 ex.getMessage();
             }
-            if(i<=200)
+            if(i<warmupNum)
                 System.out.println("JVM iteration: " + i );
             else{
-                //builds]
+                //build the display for the GUI
                 outputField.setText(sortMain.displayReport());
                 outputField.setEditable(false);
                 myJPanel.add(outputField);
